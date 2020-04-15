05a0809ed7cd8776703e4687387476ea4ad9ae7e
c14180029 <c14180029@john.petra.ac.id>
2020-04-15 09:17:02 +0700

    commit genap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,21 @@ export class AppComponent  {
     "nama":"Mainan",
     "harga":50000
   }
+  a
+  b
+  total = 0
+  output = ""
+  SaveVal(){
+    let {a,b} = this;
+    this.total = parseInt(a) + parseInt(b);
+    this.total = Math.pow(a,b);
+    if (this.total %2 ==0){
+        for(let i = b ;i<=this.total;i++){
+          for(let j = 0; j<i;j++){
+            this.output = this.output + "*"
+          }
+          this.output = this.output + "<br>"
+        }
+    }
+  }
 }
